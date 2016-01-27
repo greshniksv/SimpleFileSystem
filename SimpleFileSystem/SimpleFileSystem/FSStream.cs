fed34808c8a6c3300b2e3d2d7d7c1f0f92e34e15
GreshnikSV <greshnik-sv@yandex.ru>
2016-01-27 19:08:28 +0200

    TEst commit

---
+++ SimpleFileSystem/SimpleFileSystem/FSStream.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+
+namespace SimpleFileSystem
+{
+	class FSStream: Stream
+	{
+		public override void Flush()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override long Seek(long offset, SeekOrigin origin)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void SetLength(long value)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override int Read(byte[] buffer, int offset, int count)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override void Write(byte[] buffer, int offset, int count)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public override bool CanRead
+		{
+			get { throw new System.NotImplementedException(); }
+		}
+
+		public override bool CanSeek
+		{
+			get { throw new System.NotImplementedException(); }
+		}
+
+		public override bool CanWrite
+		{
+			get { throw new System.NotImplementedException(); }
+		}
+
+		public override long Length
+		{
+			get { throw new System.NotImplementedException(); }
+		}
+
+		public override long Position { get; set; }
+	}
+}
